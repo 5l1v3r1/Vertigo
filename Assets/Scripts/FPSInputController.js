7599f9cb684c4e1cd5b073293f3b22eef1523e83
Kevin LOCATI <kevin.locati@gmail.com>
2014-11-08 20:26:59 +0100

    added balance management, comments and rotation

--- Assets/Scripts/FPSInputController.js
+++ Assets/Scripts/FPSInputController.js
@@ -1,6 +1,31 @@
+/*
+Modified version of the FPSInputController from CharacterController
+*/
+
 private var motor : CharacterMotor;
-private var inTightRopeArea : boolean = false;
-public var nextWaypoint : Transform;
+private var inTightRopeArea : boolean = false;//is the player in a tight rope module ?
+public var balance : float = 0f;//arm balance of the player (negative: leaning left, positive: leaning right)
+public var tightRopeSpeed : float = 0.5f;//movement speed while on a tight rope
+private var balanceStep : float = 10f;//one press on A or E will modify the global balance by this amount
+
+/*
+updates the player's arm balancing
+*/
+function calculateBalance(){
+
+	//razer hydra emulation
+	if(Input.GetKeyDown(KeyCode.A)) {
+		balance += -balanceStep;
+	}
+	if(Input.GetKeyDown(KeyCode.E)) {
+		balance += balanceStep;
+	}
+	
+	//update current rotation according to the balance
+	transform.localEulerAngles.z = -balance;
+	//simulate dizziness
+	transform.Rotate(Vector3.up * Time.deltaTime * balance*balance/10);
+}
 
 // Use this for initialization
 function Awake () {
@@ -10,16 +35,17 @@ function Awake () {
 // Update is called once per frame
 function Update () {
 	// Get the input vector from keyboard or analog stick
-	
 	var directionVector : Vector3;
 	var directionLength : float;
 	
 	if(inTightRopeArea) {
-		directionVector = Vector3.forward * 0.5f;
+		directionVector = Vector3.forward * tightRopeSpeed;	
+		calculateBalance();
 	}
 	
 	else {
 		directionVector = new Vector3(-Input.GetAxis("MovementX"), 0, Input.GetAxis("MovementY"));
+		motor.inputJump = Input.GetButton("Jump");
 	}
 	
 	if (directionVector != Vector3.zero) {
@@ -39,22 +65,23 @@ function Update () {
 		directionVector = directionVector * directionLength;
 	}
 	
-	Debug.Log(directionVector);
 	// Apply the direction to the CharacterMotor
-	motor.inputJump = Input.GetButton("Jump");
 	motor.inputMoveDirection = transform.rotation * directionVector;
+	
 }
 
 function OnTriggerEnter(trigger : Collider) {
 	if(trigger.tag == "TightRopeArea") {
 		inTightRopeArea = true;
-		nextWaypoint = GameObject.Find("WayPoint").transform;
 	}
 }
 
 function OnTriggerExit(trigger : Collider) {
 	if(trigger.tag == "TightRopeArea") {
 		inTightRopeArea = false;
+		balance = 0;
+		transform.localEulerAngles.z = 0;
+		transform.localEulerAngles.x = 0;
 	}
 }
 // Require a character controller to be attached to the same game object
