64f85b0a5a20b626d59dbf57f358fd0c6ace2ca8
Kevin LOCATI <kevin.locati@gmail.com>
2014-11-09 13:58:46 +0100

    Level 1 introduced

---
+++ Assets/Scripts/Zone 0/SubwayTrigger.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(collision : Collider) {
+	if(collision.tag == "SubwayTrigger") {
+		var train : GameObject[] = GameObject.FindGameObjectsWithTag("Subway");
+		var script : Subway;
+		for(var i = 0; i < train.Length; i++) {
+			script = train[i].GetComponent("Subway");
+			script.activate();
+		}
+	}
+}
