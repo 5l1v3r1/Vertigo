14556d81e43d7963fe5a4f957687dfe5f1fcb774
Qroy <quentin.roy.fr@gmail.com>
2014-11-21 16:17:39 +0100

    improving bird generator

--- Assets/Scripts/Bird/GenerateBird.js
+++ Assets/Scripts/Bird/GenerateBird.js
@@ -1,32 +1,29 @@
 ﻿#pragma strict
 
-//public var nbBirdType: int;
-//public var arrayBird : GameObject[nbBirdType];
+// the prefab of the bird
 public var birdPrefab: GameObject;
-public var isGenerating: boolean = false;
+
+// time intervalle during generation of a bird
 public var generationInterval = 0.2f;
+
+// number of bird in the wave
+public var birdNbToGenerate : int;
 
 function Start () {
 
 }
 
 function Update () {
-	if(isGenerating){
-		GenerateBirds();
-		isGenerating = false;
-	}
+
 }
 
 public function GenerateBirds(){	
-	//while(isGenerating){
-		var difficulty:int = Random.Range(1, 4); //between 1 and 3
-
 		// generate a wave
-		for(var i: int=1; i<=difficulty; i++) 
+		for(var i: int=1; i<=birdNbToGenerate; i++) 
 		{
 			// random altitude
 			var alt:int = Random.Range(2, 5);
-			alt /= 2;	// alt between 0.5 and 1.5
+			alt /= 2;	// alt between 1, 1.5, 2
 			
 			// random side (left or right)
 			var arrayLeftOrRight = new Array(-1,-0.5, 0.5, 1);			
@@ -34,22 +31,17 @@ public function GenerateBirds(){
 			
 			// deltaPos to Instatiate Birds from the Bird Generator prefab + this deltaPos
 			var deltaPos = Vector3(leftOrRight,alt,0);
-			deltaPos = transform.rotation * deltaPos;
-
 			
-			// creation of an instance of a bird
-			var instance : GameObject = Instantiate(birdPrefab, transform.position + deltaPos, transform.rotation);
 
-			// generation of a bird every 0.2 seconds
+			var source: GameObject = transform.parent.gameObject.Find("GenerationSource");
+			deltaPos = source.transform.rotation * deltaPos;
+			// creation of an instance of a bird at the parent position
+			var instance : GameObject = Instantiate(birdPrefab, 
+													source.transform.position + deltaPos, 
+													source.transform.rotation);
+
+			// generation of a bird every "generationInterval" seconds
 			yield WaitForSeconds(generationInterval);
 		}
-		
-		// generation of a wave about every 1,5 seconds
-		//yield WaitForSeconds(2);
-	//}
 }
 
-
-public function launchGeneration(){
-	isGenerating = true;
-}
--- Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
@@ -48,23 +48,11 @@ function Update () {
 	if(inTightRopeArea) {
 		directionVector = Vector3.forward * tightRopeSpeed;	
 		calculateBalance();
-		//if(!isBirdGenerationStarted){
-		
-			// generate birds while players on the tight rope area
-			//StartCoroutine(GenerateBirds());
-			// to launch the coroutine only once
-		//	isBirdGenerationStarted = true;
-		//}
 	}
 	
 	else {
 		directionVector = new Vector3(-Input.GetAxis("MovementX"), 0, Input.GetAxis("MovementY"));
 		motor.inputJump = Input.GetButton("Jump");
-		/*if(isBirdGenerationStarted)
-		{
-//			StopCoroutine(GenerateBirds());
-			isBirdGenerationStarted = false;
-		}*/	
 	}
 	
 	if (directionVector != Vector3.zero) {
@@ -92,25 +80,9 @@ function Update () {
 function OnTriggerEnter(trigger : Collider) {
 	if(trigger.tag == "TightRopeArea") {
 		inTightRopeArea = true;
-		trigger.BroadcastMessage("launchGeneration");
-		
-		//trigger.BroadcastMessage("GenerateBirds");
-		/*currentTightRope = trigger.gameObject;
-		birdRotation = currentTightRope.transform.rotation;
-		birdStartPoint = currentTightRope.transform.position;
-		var areaScaleZ = currentTightRope.transform.localScale.z;
-		
-		// determine the angle between the player and the Passage
-		var angle =  Quaternion.Angle(transform.rotation, trigger.transform.rotation);
-		
-		//if they don't have the same Direction
-		if(angle < 90){
-			birdRotation = currentTightRope.transform.rotation * Quaternion.AngleAxis(180, Vector3.up);;
-			isSameDir = false;
-		}
-		else{
-			isSameDir = true;
-		}*/
+	}
+	if(trigger.tag == "BirdTrigger"){
+		trigger.GetComponent("GenerateBird").GenerateBirds();
 	}
 }
 
@@ -120,63 +92,10 @@ function OnTriggerExit(trigger : Collider) {
 		balance = 0;
 		transform.localEulerAngles.z = 0;
 		transform.localEulerAngles.x = 0;
-		trigger.BroadcastMessage("stopGeneration");
 	}
-}
-/*
- * Generate wave of a random number of bird flying near the player
- */
-/*function GenerateBirds(){
 	
-	while(inTightRopeArea){
-		var difficulty:int = Random.Range(1, 4); //between 1 and 3
+}
 
-		// generate a wave
-		for(var i: int=1; i<=difficulty; i++) 
-		{
-			// random altitude
-			var alt:int = Random.Range(1, 4);
-			//alt /=2;	// alt between 0.5 and 1.5
-			
-			// random side (left or right)
-			var arrayLeftOrRight = new Array(-0.5, 0.5);			
-			var leftOrRight = arrayLeftOrRight[Random.Range(0, arrayLeftOrRight.length)];
-			
-			// z dimension of the TightRopeArea
-			var areaScaleZ = currentTightRope.transform.parent.transform.localScale.z;
-			// rotation of the TightRopeArea
-			var rot = currentTightRope.transform.rotation.eulerAngles;
-			
-			// deltaPos to Instatiate Birds from the middle of the TightRopeArea + this deltaPos
-			var deltaPos = Vector3((areaScaleZ/2)*( Mathf.Cos(rot.x*Mathf.Deg2Rad)*-Mathf.Sin(rot.y*Mathf.Deg2Rad)),
-				     			   		(areaScaleZ/2)*( Mathf.Sin(rot.x*Mathf.Deg2Rad)) ,
-				     			   		(areaScaleZ/2)*(Mathf.Cos(rot.x*Mathf.Deg2Rad)*(-Mathf.Cos(rot.y*Mathf.Deg2Rad)))
-				     			   	);
-			//some operations if the TightRopeArea and the player don't have the same orientation
-			if(!isSameDir){
-				deltaPos.z = -deltaPos.z;
-				deltaPos.y = -deltaPos.y;
-				deltaPos.x = -deltaPos.x;
-			}
-			
-			// some operations to instantiate birds at random positions
-			deltaPos.y +=alt;
-			deltaPos.x += leftOrRight;
-			deltaPos.z += leftOrRight;
-			
-			// creation of an instance of a bird
-			var instance : GameObject = Instantiate(	birdPrefab, 
-				     			   	currentTightRope.transform.parent.transform.position 
-				     			   	+ deltaPos
-				     			   	,birdRotation);
-			// generation of a bird every 0.2 seconds
-			yield WaitForSeconds(0.2);
-		}
-		
-		// generation of a wave about every 1,5 seconds
-		yield WaitForSeconds(1.5);
-	}
-}*/
 
 // Require a character controller to be attached to the same game object
 @script RequireComponent (CharacterMotor)
